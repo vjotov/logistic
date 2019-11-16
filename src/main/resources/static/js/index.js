b54c328e9af3d91bd9343b40dbb71088afdd9691
Vladimir Jotov <v.jotov@gmail.com>
2019-11-17 00:58:59 +0700

    save

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -14,16 +14,44 @@ function buildRoute(view) {
     }
 }
 
-require(['views/main', 'views/cars'], function(main, cars) {
+function buildButton(label, route) {
+    return {
+               view:"button",
+               vslue:label,
+               width:100,
+               align: "center",
+               click: function() {
+                    routie(route)
+               }
+           }
+}
+
+require(
+    ['views/main', 'views/cars', 'view/marks'],
+    function(main, cars, marks) {
     webix.ready(function() {
         webix.ui({
-            id: 'root',
-            container: "app"
+            container: "app",
+            width: document.body.clientQidth,
+            height: document.body.clientHeight,
+            rows: [
+                {
+                    view: "toolbar",
+                    cols: [
+                        buildButton("Home", ""),
+                        buildButton("Marks", "marks")
+                    ]
+                },
+                {
+                    id: 'root'
+                }
+             ]
         })
     })
 
     routie({
         '': buildRoute(main),
-        'cars': buildRoute(cars)
+        'cars': buildRoute(cars),
+        'marks':buildRoute(marks)
     })
 })
